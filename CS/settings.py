3ecc12ef24af7005a41e4f3060dc412ea9b27da5
Jorge Alejandro Altúzar Guillén <183391@ids.upchiapas.edu.mx>
2020-02-07 17:20:45 -0600

    DecouplePython Commit

--- CS/settings.py
+++ CS/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n@c+nbc%7z9v!820gj84+$^-2c7@#&m65gpp-n_)@7a(9l53*_'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost','34.238.249.110']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast = Csv())
 
 
 # Application definition
@@ -94,10 +95,10 @@ WSGI_APPLICATION = 'CS.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myawsdbcs',
-        'USER' : 'postgres',
-        'PASSWORD' : 'jaag2610',
-		'HOST' : 'myawscs.ccaw9xkntxqz.us-east-1.rds.amazonaws.com',
+        'NAME': config('DB_NAME'),
+        'USER' : config('DB_USER'),
+        'PASSWORD' : config('DB_PASSWORD'),
+		'HOST' : config('DB_HOST'),
 		'PORT' : '5432'
     }
 }
@@ -134,6 +135,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
 
 
 # Static files (CSS, JavaScript, Images)
