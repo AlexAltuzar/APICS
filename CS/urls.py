b1e868210b8e1d0bf9343ad3683bfd384d30662f
Jorge Alejandro Altúzar Guillén <183391@ids.upchiapas.edu.mx>
2020-02-10 11:38:15 -0600

    BDExample2

--- CS/urls.py
+++ CS/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls import include
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 
+
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -35,5 +36,6 @@ router.register(r'users', UserViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^',include(router.urls)),
-    re_path(r'^api/v1/login',include('Login.urls')),
+    re_path(r'^api/v1/',include('Login.urls')),
+    
 ]
